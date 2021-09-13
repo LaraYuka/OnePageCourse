2fef855d538b4c8dfc46a2bba37b979e1d427017
Lara Sakanaka <85855158+LaraYuka@users.noreply.github.com>
2021-09-13 13:48:49 +0000

    Aula do dia 13/9/2021

--- js/menu.js
+++ js/menu.js
@@ -1,6 +1,29 @@
 $(document).ready(function(){
+
   $('#btn-menu').click(function(e){
     e.preventDefault()
     $('#menu').toggle('slow')
   })
+
+  //Pegando os link do menu para apresentar a página
+
+  //$('a').click(function(e){
+    //e.preventDefault()
+
+    //Variavel para se guardar qual o link abrir
+
+    //let url = $(this).attr('href')
+    //$('#'+url).show()
+
+  //})
+
+  //Testando:
+  $('#home').click(function(e){
+    e.preventDefault()
+    $('href').removeId('#contact')
+
+    let url = $(this).attr('href')
+    $('#home'+url).show()
+  })
+  
 })
