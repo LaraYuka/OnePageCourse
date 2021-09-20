7341b55b9bad31915695d0940d92f1a56c4e603a
Lara Sakanaka <85855158+LaraYuka@users.noreply.github.com>
2021-09-20 19:36:54 +0000

    finalização 20/9/2021

--- js/menu.js
+++ js/menu.js
@@ -16,14 +16,72 @@ $(document).ready(function(){
     //$('#'+url).show()
 
   //})
+  
 
   //Testando:
-  $('#home').click(function(e){
+
+  //$('#home').click(function(e){
+    //e.preventDefault()
+    //$('href').removeId('#contact')
+
+    //let url = $(this).attr('href')
+    //$('#home'+url).show()
+  //})
+
+  $('a').click(function(e){
     e.preventDefault()
-    $('href').removeId('#contact')
+
+    //Variavel para se guardar qual o link abrir
+
+    //if(let url = $(this).attr('href')){
+      //$('#'+url).show()
+    //}
 
     let url = $(this).attr('href')
-    $('#home'+url).show()
+    //$('#'+url).show()
+
+    if(url === 'about'){
+      $('#'+url).show()
+
+      $("#contact").hide()
+      $("#home").hide()
+      $("#action").hide()
+
+    } 
+    else if(url === 'home'){
+      $('#'+url).show()
+
+      $("#about").hide()
+      $("#contact").hide()
+      $("#action").hide()
+
+    } 
+    else if(url === 'action'){
+      $('#'+url).show()
+
+      $("#about").hide()
+      $("#contact").hide()
+      $("#home").hide()
+
+    }
+    else if(url === 'contact'){
+      $('#'+url).show()
+
+      //$('a').remove(url, 'about')
+      //$("id").removeAttr("#about"); 
+      //$("a").remove("#about")
+      //$("#about").remove()
+      //$("#home").remove()
+      //$("#action").remove()
+
+      //$("#contact").add()
+
+      $("#about").hide()
+      $("#home").hide()
+      $("#action").hide()
+      
+    }
+
   })
   
 })
